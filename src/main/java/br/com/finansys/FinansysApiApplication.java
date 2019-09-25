865aabe43553291bcd45f3758f32bef7a0b67a81
Marcelo <marcelojunin2010@hotmail.com>
2019-09-24 23:35:33 -0300

    Creating fake data to category.

--- src/main/java/br/com/finansys/FinansysApiApplication.java
+++ src/main/java/br/com/finansys/FinansysApiApplication.java
@@ -1,13 +1,30 @@
 package br.com.finansys;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.finansys.domain.Category;
+import br.com.finansys.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class FinansysApiApplication {
+public class FinansysApiApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinansysApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		for (int i = 0; i < 10; i++) {
+			categoryRepository.save(new Category(null, "Category " + (i + 1), "bla bla bla " + (i + 1)));
+		}
+ 		
 	}
 
 }
